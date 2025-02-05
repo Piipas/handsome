782661b2303d1d61750d15d6fb5e3f2136414167
Pipas <ismailpipas@gmail.com>
2025-02-05 13:52:06 +0100

    feat: add compliment feature and share button; update styles and dependencies

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,36 @@
 const nameValue = $(".name-value");
 const nameInput = $(".name-input")[0];
 let inputValueLength = nameInput.value.length;
+const complimentValue = $(".compliment");
+
+const complimentValues = [
+  {
+    gender: "male",
+    compliment: "handsome",
+    color: "#87CEEB",
+    foreground: "#FD79A8",
+    emoji: "😎",
+  },
+  {
+    gender: "female",
+    compliment: "Gorgeous",
+    color: "#FD79A8",
+    foreground: "#87CEEB",
+    emoji: "😍",
+  },
+];
+
+const url = new URL(window.location.href);
+const gender = url.searchParams.get("gender");
+const locked = url.searchParams.get("locked");
+
+const currentComplimentValue = complimentValues.find((compliment) =>
+  gender ? compliment.gender == gender : compliment.gender == "male",
+);
+
+complimentValue.text(currentComplimentValue.compliment);
+$("body").css("background-color", currentComplimentValue.color);
+$(".emoji").text(currentComplimentValue.emoji);
 
 $(".name-value").on("click", () => {
   nameInput.selectionStart = inputValueLength;
@@ -16,7 +46,7 @@ $(".name-input").on("keyup", (e) => {
   url.searchParams.set("name", currentValue);
   window.history.pushState(null, "", url);
 
-  if (!nameInput.value.length) $(".click-here").fadeIn();
+  if (!nameInput.value.length || !locked) $(".click-here").fadeIn();
   else $(".click-here").fadeOut();
 });
 
@@ -30,3 +60,32 @@ $(".name-input").on("blur", () => {
 
 if (!nameInput.value.length) $(".click-here").show();
 else $(".click-here").hide();
+
+$(".compliment").on("click", () => {
+  const url = new URL(window.location.href);
+  const gender = url.searchParams.get("gender");
+  const currentComplimentValue = complimentValues.find((compliment) =>
+    gender ? compliment.gender == gender : compliment.gender == "male",
+  );
+
+  const currentComplimentValueIndex = complimentValues.findIndex(
+    (compliment) => compliment.compliment === currentComplimentValue.compliment,
+  );
+  const nextComplimentindex =
+    currentComplimentValueIndex + 1 >= complimentValues.length ? 0 : currentComplimentValueIndex + 1;
+
+  url.searchParams.set("gender", complimentValues[nextComplimentindex].gender);
+  window.history.pushState(null, "", url);
+
+  $("body").css("background-color", complimentValues[nextComplimentindex].color);
+  $(".emoji").text(complimentValues[nextComplimentindex].emoji);
+  complimentValue.text(complimentValues[nextComplimentindex].compliment);
+});
+
+$(".share").on("click", () => {
+  console.log(nameValue);
+  const url = new URL(window.location.href);
+  // url.searchParams.set("locked", "true");
+  navigator.clipboard.writeText(url.href);
+  alert(`The link has been successfully copied! Go ahead and share it with ${url.searchParams.get("name")}.`);
+});
